ef3d89d5d27ecff57df07cf71c0010a11f5bff45
icchigoo <icchigoo12@gmail.com>
2023-03-13 23:29:10 +0545

    intial setup

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Header = () => {
+  return <>
+  <header className="header-top-strip">
+    
+
+  </header>
+  </>;
+};
+
+export default Header;
